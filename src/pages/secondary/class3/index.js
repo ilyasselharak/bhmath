104a2b7840d8638478f8eaf86627fe7a488f848a
ilyass el harak <ilyasselharak@gmail.com>
2023-07-24 21:44:09 +0300

    fixed

--- src/pages/secondary/class3/index.js
+++ src/pages/secondary/class3/index.js
@@ -1,7 +1,6 @@
 import MainHeader from "@/components/MainHeader";
 import NavHeader from "@/components/NavHeader";
 import TopHeader from "@/components/TopHeader";
-import Link from "next/link";
 import React from "react";
 
 export default function index() {
@@ -11,24 +10,29 @@ export default function index() {
       <MainHeader />
       <NavHeader />
       <main className="border-2 border-green-800 p-3 mt-8">
-        <h4 className="font-bold text-xl">
-          دروس وتمارين وفروض، امتحانات لتلاميذ الثانوي الإعدادي
-        </h4>
+        <h4 className="font-bold text-xl">دروس وتمارين وفروض الجذع المشترك</h4>
         <p className="mt-3">
-          دروس وتمارين وفروض، امتحانات لتلاميذ التعليم الثانوي الإعدادي.
+          دروس وتمارين وفروض محروسة وفروض منزلية لتلاميذ الجذع المشترك، للدورتين
+          الأولى والثانية وفق مقرر وزارة التربية الوطنية المغربية.
         </p>
         <div className="mt-4 bg-green-400 py-2 text-white pr-2">
           المرجو اختيار قسم فرعي:
         </div>
         <div className="grid grid-cols-1 mt-4 md:grid-cols-3 gap-8">
           <div className="bg-green-300 text-center py-3 rounded-md hover:bg-green-600 cursor-pointer">
-            <Link href="/secondary/class1">الجذع المشترك</Link>
+            SVT/PC
           </div>
           <div className="bg-green-300 text-center py-3 rounded-md hover:bg-green-600 cursor-pointer">
-            <Link href="/secondary/class2">اولى باكالوريا</Link>
+            Math
           </div>
           <div className="bg-green-300 text-center py-3 rounded-md hover:bg-green-600 cursor-pointer">
-            ثانية باكالوريا
+            أولى باك اداب و علوم انسانية
+          </div>
+          <div className="bg-green-300 text-center py-3 rounded-md hover:bg-green-600 cursor-pointer">
+            أولى باك تكنولوجي
+          </div>
+          <div className="bg-green-300 text-center py-3 rounded-md hover:bg-green-600 cursor-pointer">
+            أولى باك اقتصاد
           </div>
         </div>
       </main>
