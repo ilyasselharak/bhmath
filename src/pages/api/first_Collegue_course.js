95b528dff659b6741b122d46318fa82f9561fe51
ilyass el harak <ilyasselharak@gmail.com>
2023-08-31 14:10:11 +0300

    fixed

--- src/pages/api/first_Collegue_course.js
+++ src/pages/api/first_Collegue_course.js
@@ -1,5 +1,8 @@
 import Second_Collegue_Course from "@/modules/SecondCollegueCourses";
 import Third_Collegue_Course from "@/modules/ThirdCollegueCourses";
+import Second_Collegue_Devoire from "@/modules/devoiresSecondCollegue";
+import First_Collegue_Devoire from "@/modules/devoiresfirstcollegue";
+import Third_Collegue_Devoire from "@/modules/devoiresthirdCollegue";
 import First_Collegue_Course from "@/modules/firstCollegueCourses";
 import First_Lycee_Course from "@/modules/firstLyceeCourses";
 
@@ -13,6 +16,12 @@ export async function find_Second_Collegue_Course() {
 export async function find_Third_Collegue_Course() {
   return Third_Collegue_Course.find().exec();
 }
-export async function find_First_Lycee_Course() {
-  return First_Lycee_Course.find().exec();
+export async function find_First_Collegue_Devoire(){
+  return First_Collegue_Devoire.find().exec()
+}
+export async function find_Second_Collegue_Devoire(){
+  return Second_Collegue_Devoire.find().exec()
+}
+export async function find_Third_Collegue_Devoire(){
+  return Third_Collegue_Devoire.find().exec()
 }
