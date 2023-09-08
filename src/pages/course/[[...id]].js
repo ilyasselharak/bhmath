d47cd458b3580a3554b6904fa37eaa4f5295d0e0
ilyass el harak <ilyasselharak@gmail.com>
2023-09-08 21:18:08 +0300

    fixed

--- src/pages/course/[[...id]].js
+++ src/pages/course/[[...id]].js
@@ -4,6 +4,10 @@ import React from 'react'
 import { find_First_Collegue_Course, find_Second_Collegue_Course, find_Third_Collegue_Course, find_Third_Secondary_Courses } from '../api/first_Collegue_course';
 import Link from 'next/link';
 import { find_First_Secondary_TCT_Course, find_Second_Secondary_Course, find_Second_Secondary_Math_Course } from '../api/first_Secondary_course';
+import TopHeader from '@/components/TopHeader';
+import MainHeader from '@/components/MainHeader';
+import NavHeader from '@/components/NavHeader';
+import Image from 'next/image';
 
 
 
@@ -54,13 +58,26 @@ export default function index({course,filtred}) {
   
    const router= useRouter()
   return (
+    <>
+    <TopHeader/>
+    <MainHeader />
+    <NavHeader />
     <div >
        
         
        {filtred.length ? 
-       <div className='flex flex-col items-center gap-y-12'>{filtred[0].courseLink.split(",,").map((item,index)=>{return <iframe key={index} src={item.trim()} style={{width:"80%"}} height="780" allow="autoplay" ></iframe>})} </div>
+       <div className='flex flex-col items-center gap-y-12'>
+        {filtred[0].courseLink.split(",,").map((item,index)=>{
+          return <iframe key={index} src={item.trim()} style={{width:"80%"}} height="780" allow="autoplay" ></iframe>}
+          )}
+         </div>
         :
-        <div className='flex flex-col gap-y-5 '>{course.map((item,index)=><Link key={index} href={`${router.asPath}/${item.name}`}><div  className=' bg-yellow-200 p-4 rounded-md '>{item.name}</div></Link>)}</div>}
+<div className=' flex justify-between mt-16'>
+  <Image src={'/ads600.jpg'} width={160} height={600} alt={"addsenceAds"}/>
+        <div className='flex flex-col gap-y-5  uppercase '>{course.map((item,index)=><Link key={index} href={`${router.asPath}/${item.name}`}><div  className=' bg-yellow-200 p-4 rounded-md text-center'>{item.name}</div></Link>)}</div>
+        <Image src={'/ads600.jpg'} width={160} height={600} alt={"addsenceAds"}/>
+        </div>}
     </div>
+    </>
   )
 }
